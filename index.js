2c459bd24c74efed6ccb04038241e29b7ddf2c71
Sumit Govind Chougule <talknikit@gmail.com>
2026-04-07 14:28:46 +0530

    Switching to English text to fix encoding

---
+++ index.js
@@ -0,0 +1,10 @@
+﻿const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+    res.send('Talknik IT API welcomes you!');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log('Server running on port ' + PORT));
