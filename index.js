e9b3125e84ad187e09aba2335f090692671fd1ce
Sumit Govind Chougule <talknikit@gmail.com>
2026-04-08 13:07:36 +0530

    Fix: Stable Connection String

--- index.js
+++ index.js
@@ -7,14 +7,15 @@ const PORT = process.env.PORT || 10000;
 app.use(cors());
 app.use(express.json());
 
+// ????? ?????? ??????? ????????
 const MONGO_URI = "mongodb+srv://talknikhindi_db_user:JfdirClPXXXjHyBq@cluster0.svqt5mp.mongodb.net/talknik_db?retryWrites=true&w=majority";
 
 const AdSchema = new mongoose.Schema({ network: String, code: String });
 const Ad = mongoose.model('Ad', AdSchema);
 
-mongoose.connect(MONGO_URI)
-    .then(() => console.log('? DB Connected'))
-    .catch(err => console.error('? DB Error:', err));
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('? MongoDB Connected Successfully'))
+    .catch(err => console.error('? Connection Error:', err));
 
 app.get('/', (req, res) => res.send('Talknik Engine Active ??'));
 
@@ -33,4 +34,4 @@ app.get('/get-active-ad', async (req, res) => {
     } catch (e) { res.send(''); }
 });
 
-app.listen(PORT, () => console.log('?? Server running on ' + PORT));
+app.listen(PORT, () => console.log('?? Server is running!'));
