776e33d0a6b4e2de9cf13aad59ba0a0d186352f2
Sumit Govind Chougule <talknikit@gmail.com>
2026-04-08 13:11:23 +0530

    Fix: Final DB Stability

--- index.js
+++ index.js
@@ -7,12 +7,15 @@ const PORT = process.env.PORT || 10000;
 app.use(cors());
 app.use(express.json());
 
-const MONGO_URI = "mongodb+srv://talknikhindi_db_user:JfdirClPXXXjHyBq@cluster0.svqt5mp.mongodb.net/talknik_db?retryWrites=true&w=majority";
+// ????? ??????? ????????
+const URI = "mongodb+srv://talknikhindi_db_user:JfdirClPXXXjHyBq@cluster0.svqt5mp.mongodb.net/talknik_db?retryWrites=true&w=majority";
 
 const AdSchema = new mongoose.Schema({ network: String, code: String });
 const Ad = mongoose.model('Ad', AdSchema);
 
-mongoose.connect(MONGO_URI).then(() => console.log('? DB Connected')).catch(err => console.log('? Error:', err));
+mongoose.connect(URI)
+    .then(() => console.log('? DB Connected'))
+    .catch(err => console.log('? DB Error:', err));
 
 app.get('/', (req, res) => res.send('Talknik Engine Active ??'));
 
@@ -20,15 +23,15 @@ app.post('/update-ads', async (req, res) => {
     try {
         const { network, code } = req.body;
         await Ad.findOneAndUpdate({ network: 'active' }, { network, code }, { upsert: true });
-        res.json({ message: '? Ad Updated' });
+        res.json({ message: '? Updated' });
     } catch (e) { res.status(500).send(e.message); }
 });
 
 app.get('/get-active-ad', async (req, res) => {
     try {
         const data = await Ad.findOne({ network: 'active' });
-        res.send(data ? data.code : 'No Ads');
+        res.send(data ? data.code : '');
     } catch (e) { res.send(''); }
 });
 
-app.listen(PORT, () => console.log('?? Server running'));
+app.listen(PORT, () => console.log('?? Engine Live'));
