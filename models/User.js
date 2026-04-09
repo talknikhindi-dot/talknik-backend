666b4c9545fe03db32a9b58b7c50da7834d182d9
Sumit Govind Chougule <talknikit@gmail.com>
2026-04-09 11:00:28 +0530

    Final Super Fast Deploy

--- models/User.js
+++ models/User.js
@@ -1,9 +1,6 @@
 ﻿const mongoose = require('mongoose');
-
-const UserSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now }
+    password: { type: String, required: true }
 });
-
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model('User', userSchema);
