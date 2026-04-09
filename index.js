79e81016d8fc0778535327b3a8cb37878afcf842
Sumit Govind Chougule <talknikit@gmail.com>
2026-04-09 12:23:13 +0530

    Full Sync: Updated MongoDB URI with talknik2026 password

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ app.use(express.json());
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// थेट रेंडरवरून घेतलेला पासवर्ड वापरला आहे
-const mongoURI = "mongodb+srv://talknikhindi_db_user:JFdirClPXKXjHyBq@cluster0.svqt5mp.mongodb.net/talknik_db?retryWrites=true&w=majority";
+// नवीन पासवर्ड 'talknik2026' अपडेट केला आहे
+const mongoURI = "mongodb+srv://talknikhindi_db_user:talknik2026@cluster0.svqt5mp.mongodb.net/talknik_db?retryWrites=true&w=majority";
 
 mongoose.connect(mongoURI)
     .then(() => console.log('✅ Talknik DB Connected Successfully!'))
     .catch((err) => console.log('❌ DB Connection Error: ', err.message));
 
-app.get('/', (req, res) => res.send('Talknik Engine Live!'));
+app.get('/', (req, res) => res.send('🚀 Talknik IT Engine is Live & Connected!'));
 
 const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log('🚀 Talknik Engine running on ' + PORT));
